19cb2325530bd482fa1cba1a80fbc465b0f9094c
EDUARDO <EDUARDO@egil-laptop>
2017-03-10 11:08:48 -0500

    Create new report with sales statistics

---
+++ src-fiscal/com/ghintech/fiscalprint/PrintXReportSummary.java
@@ -0,0 +1,59 @@
+//    Openbravo POS is a point of sales application designed for touch screens.
+//    Copyright (C) 2007-2009 Openbravo, S.L.
+//    http://www.openbravo.com/product/pos
+//
+//    This file is part of Openbravo POS.
+//
+//    Openbravo POS is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Openbravo POS is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Openbravo POS.  If not, see <http://www.gnu.org/licenses/>.
+
+package com.ghintech.fiscalprint;
+
+import com.openbravo.pos.forms.AppView;
+import com.openbravo.pos.forms.BeanFactoryCache;
+import com.openbravo.pos.forms.BeanFactoryException;
+import com.openbravo.pos.forms.DataLogicSystem;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * @author Eduardo Gil
+ */
+public class PrintXReportSummary extends BeanFactoryCache {
+    
+    private DataLogicFiscal dlFiscal;
+    @Override
+    public Object constructBean(AppView app) throws BeanFactoryException {
+        DataLogicSystem dlSystem = (DataLogicSystem) app.getBean("com.openbravo.pos.forms.DataLogicSystem");
+        dlFiscal = (DataLogicFiscal) app.getBean("com.ghintech.fiscalprint.DataLogicFiscal");
+        PrintReport bean = new PrintReport("I0X\n"+createSummary(),dlSystem);
+        
+        return bean;
+    }
+
+    private String createSummary() {
+        
+        String rep ;
+        rep=dlFiscal.salesByProduct();
+        rep+=dlFiscal.salesByCategory();
+        rep+=dlFiscal.salesByTaxes();
+        rep+=dlFiscal.salesByHour();
+        rep+=dlFiscal.totalTips();
+        
+        return rep;
+    }
+
+    
+   
+}
